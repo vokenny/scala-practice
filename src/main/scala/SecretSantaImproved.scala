ca443ef435fdf72dca27d3102e9dcb30fbe8bc41
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2019-11-20 10:21:34 +0000

    Done Secret Santa App challenge

---
+++ src/main/scala/SecretSantaImproved.scala
@@ -0,0 +1,44 @@
+import scala.annotation.tailrec
+import scala.io.StdIn._
+import scala.language.postfixOps
+import scala.util.Random
+
+object SecretSantaImproved extends App {
+
+  // Will stop at size of the list, because each pair is assigned one by one methodically
+
+  def createListOfNames(): List[String] = {
+    println("How many people are playing Secret Santa?")
+    val numOfPpl: Int = readInt()
+
+    List.fill(numOfPpl)(getNames)
+  }
+
+  def getNames: String = readLine("Add a name to the Secret Santa list\n")
+
+  def randomSelect(list: List[String]): String = list(Random.nextInt(list.size))
+
+  def pairNames(names: List[String]): Map[String, String] = {
+    @tailrec
+    def pair(gifters: List[String], giftees: List[String], pairs: Map[String, String]): Map[String, String] = {
+      gifters match {
+        case Nil => pairs
+        case gifter :: Nil => pairs + (gifter -> giftees.head)
+        case gifter :: remainGifters =>
+          val giftee = randomSelect(giftees.filter(_ != gifter))
+          pair(remainGifters, giftees.filter(_ != giftee), pairs + (gifter -> giftee))
+      }
+    }
+
+    pair(names, names, Map())
+  }
+
+  def showResult(giftPairs: Map[String, String]): Unit = giftPairs.foreach(person => println(s"${person._1} will give a gift to ${person._2}"))
+
+  val listOfNames: List[String] = createListOfNames()
+  val nameMap: Map[String, String] = pairNames(listOfNames)
+  showResult(nameMap)
+
+  //Test
+  //showResult(pairNames((1 to 10).map(e => e.toString).toList))
+}

8b6167e82a53101cd2bc1bb3749535e8bbd2ec6f
Kenny Vo <kennyvo@kennys-mbp.home>
2020-02-18 14:59:11 +0000

    Coding Kata FizzBuzz

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,3 +1,16 @@
-object FizzBuzz {
+object FizzBuzz extends App {
 
+//  For a given range of numbers, print them all out
+//  where all numbers divisible by three are replaced with "Fizz"
+//  and all numbers divisible by five are replaced with "Buzz"
+//  and all numbers divisible by both are replaced with "FizzBuzz"
+
+  def fizzBuzzMate(numbers: Range): List[Any] = {
+    numbers.map {
+      case i if (i % 3 == 0) && (i % 5 == 0) => "FizzBuzz"
+      case i if i % 3 == 0 => "Fizz"
+      case i if i % 5 == 0 => "Buzz"
+      case i => i
+    }.toList
+  }
 }
--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -1,3 +1,9 @@
-class FizzBuzzSpec {
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
+class FizzBuzzSpec extends AnyFlatSpec with Matchers {
+
+  "fizzBuzzMate" should "return correct list of numbers with Fizzes and Buzzes" in {
+    FizzBuzz.fizzBuzzMate(1 to 15) should be (List(1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"))
+  }
 }
